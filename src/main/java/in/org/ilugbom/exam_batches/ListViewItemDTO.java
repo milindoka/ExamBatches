4a3f05dd4ae63061d12e78e82cf20626365baf38
Milind Oka <oak444@gmail.com>
2019-02-04 21:37:38 +0530

    Basic ListView with checkbox items

---
+++ src/main/java/in/org/ilugbom/exam_batches/ListViewItemDTO.java
@@ -0,0 +1,23 @@
+package in.org.ilugbom.exam_batches;
+public class ListViewItemDTO {
+
+    private boolean checked = false;
+
+    private String itemText = "";
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public String getItemText() {
+        return itemText;
+    }
+
+    public void setItemText(String itemText) {
+        this.itemText = itemText;
+    }
+}
---
+++ src/main/java/in/org/ilugbom/exam_batches/MainActivity.java
@@ -0,0 +1,297 @@
+package in.org.ilugbom.exam_batches;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+/////////////////////////////////////////////////////----///////////
+
+        // Get listview checkbox.
+        final ListView listViewWithCheckbox = (ListView)findViewById(R.id.list_view_with_checkbox);
+
+        // Initiate listview data.
+        final List<ListViewItemDTO> initItemList = this.getInitViewItemDtoList();
+
+        // Create a custom list view adapter with checkbox control.
+       // In the foloowing line Original ApplicationContext() gave white color to listview
+        // Changed to getBaseContex()
+        final ListViewItemCheckboxBaseAdapter listViewDataAdapter = new ListViewItemCheckboxBaseAdapter(getBaseContext(), initItemList);
+
+        listViewDataAdapter.notifyDataSetChanged();
+
+        // Set data adapter to list view.
+        listViewWithCheckbox.setAdapter(listViewDataAdapter);
+
+        // When list view item is clicked.
+        listViewWithCheckbox.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int itemIndex, long l) {
+                // Get user selected item.
+                Object itemObject = adapterView.getAdapter().getItem(itemIndex);
+
+                // Translate the selected item to DTO object.
+                ListViewItemDTO itemDto = (ListViewItemDTO)itemObject;
+
+                // Get the checkbox.
+                CheckBox itemCheckbox = (CheckBox) view.findViewById(R.id.list_view_item_checkbox);
+
+                // Reverse the checkbox and clicked item check state.
+                if(itemDto.isChecked())
+                {
+                    itemCheckbox.setChecked(false);
+                    itemDto.setChecked(false);
+                }else
+                {
+                    itemCheckbox.setChecked(true);
+                    itemDto.setChecked(true);
+                }
+
+                //Toast.makeText(getApplicationContext(), "select item text : " + itemDto.getItemText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Click this button to select all listview items with checkbox checked.
+        Button selectAllButton = (Button)findViewById(R.id.list_select_all);
+        selectAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int size = initItemList.size();
+                for(int i=0;i<size;i++)
+                {
+                    ListViewItemDTO dto = initItemList.get(i);
+                    dto.setChecked(true);
+                }
+
+                listViewDataAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // Click this button to disselect all listview items with checkbox unchecked.
+        Button selectNoneButton = (Button)findViewById(R.id.list_select_none);
+        selectNoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int size = initItemList.size();
+                for(int i=0;i<size;i++)
+                {
+                    ListViewItemDTO dto = initItemList.get(i);
+                    dto.setChecked(false);
+                }
+
+                listViewDataAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // Click this button to reverse select listview items.
+        Button selectReverseButton = (Button)findViewById(R.id.list_select_reverse);
+        selectReverseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int size = initItemList.size();
+                for(int i=0;i<size;i++)
+                {
+                    ListViewItemDTO dto = initItemList.get(i);
+
+                    if(dto.isChecked())
+                    {
+                        dto.setChecked(false);
+                    }else {
+                        dto.setChecked(true);
+                    }
+                }
+
+                listViewDataAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // Click this button to remove selected items from listview.
+        Button selectRemoveButton = (Button)findViewById(R.id.list_remove_selected_rows);
+        selectRemoveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setMessage("Are you sure to remove selected listview items?");
+
+                alertDialog.setButton(Dialog.BUTTON_POSITIVE, "Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        int size = initItemList.size();
+                        for(int i=0;i<size;i++)
+                        {
+                            ListViewItemDTO dto = initItemList.get(i);
+
+                            if(dto.isChecked())
+                            {
+                                initItemList.remove(i);
+                                i--;
+                                size = initItemList.size();
+                            }
+                        }
+
+                        listViewDataAdapter.notifyDataSetChanged();
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    // Return an initialize list of ListViewItemDTO.
+    private List<ListViewItemDTO> getInitViewItemDtoList()
+    {
+        String itemTextArr[] = {"Android", "iOS", "Java", "JavaScript", "JDBC", "JSP", "Linux", "Python", "Servlet", "Windows"};
+
+        List<ListViewItemDTO> ret = new ArrayList<ListViewItemDTO>();
+
+        int length = itemTextArr.length;
+
+        for(int i=0;i<length;i++)
+        {
+            String itemText = itemTextArr[i];
+
+            ListViewItemDTO dto = new ListViewItemDTO();
+            dto.setChecked(false);
+            dto.setItemText(itemText);
+
+            ret.add(dto);
+        }
+
+        return ret;
+    }
+
+
+
+
+
+
+}
